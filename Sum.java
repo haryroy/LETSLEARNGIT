ea29e690e34ac8ab5223ba59de5a4ebe2fb4db98
Hary Roy <hirabrothers@gmail.com>
2023-02-05 12:13:35 +0530

    today Update

--- Sum.java
+++ Sum.java
@@ -2,5 +2,6 @@ public class Sum{
     public static void main(String [] args) {
         System.out.println("Atama Namaste");
         System.out.println("13:00");
+        System.out.println("Hello Ji");
     }
 }
